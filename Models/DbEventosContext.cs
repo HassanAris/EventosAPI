82ebfcbb73c566c4d9d0557ff0a44e67758eab3c
HassanAris <hassanarisneto@gmail.com>
2024-12-12 02:41:30 -0300

    alteração config notebook

--- Models/DbEventosContext.cs
+++ Models/DbEventosContext.cs
@@ -31,7 +31,7 @@ public partial class DbEventosContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=HASSAN\\SQLEXPRESS;Database=dbEventos;Trusted_Connection=True;\nTrustServerCertificate=True;");
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-V4VFQ0M\\SQLEXPRESS;Database=dbEventos;Trusted_Connection=True;\nTrustServerCertificate=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
